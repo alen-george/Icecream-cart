14f82eb4e1461c73630e4f8a045d2c446cfa526d
Alen George <al3ngeorge@gmail.com>
2022-05-09 09:23:15 +0530

    intial shopping-cart

---
+++ api/Product.js
@@ -0,0 +1,32 @@
+const productsList = {
+  1:{
+
+      price: 90,
+      name: "Vanilla Icecream Scoop",
+      link: "/images/images-choco.jpeg",
+    },
+  2:{
+   
+      price: 122,
+      name: "ButterScotch Icebar",
+      link: "/images/Indian-Butterscotch-Ice-Cream.jpg",
+    },
+    3:{
+      price: 80,
+      name: "Chocolate Icecream",
+      link: "/images/images-choco.jpeg",
+    },
+   4: {
+    
+      price: 115,
+      name: "PowderKeg Custord",
+      link: "/images/images-custard.jpeg",
+    },
+    5:{
+      price: 210,
+      name: "Frozen Peanut Bar Icecream",
+      link: "/images/peanut-Butter-Ice-Cream-Bars.jpg",
+    },
+};
+
+  module.exports=productsList
---
+++ api/routes/testAPI.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var router = express.Router();
+const productsList = require("../Product");
+
+let cartItems=[];
+
+router.get("/", function (req, res, next) {
+  res.send({
+    code: 200,
+    response: {
+      data: productsList,
+    },
+  });
+  cartItems=[]
+});
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+router.post("/", (request, response) => {
+  cartItems = {items:{...request.body.items},cart:{...cartItems.cart,...request.body.cart}};
+  console.log(cartItems);
+  response.send({ message: "Cart updated in server" });
+});
+
+router.get("/cart", (req, res) => {
+  if (Object.keys(cartItems).length) {
+    res.json(cartItems);
+  } else {
+    res.status(400).json({ msg: "Empty Cart! Please add items" });
+  }
+});
+
+module.exports = router;
